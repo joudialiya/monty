cd75ce78ae48ac6ce195573467e0cc6a9779611a
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-20 07:40:50 -0700

    4. adapte add, swap callbacks to free the globle var at failure

--- pop_callback.c
+++ pop_callback.c
@@ -6,7 +6,7 @@ void pop_callback(stack_t **head, unsigned int line_number)
 	if(*head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
-		free_stack(head);
+		free_state(g_state);
 		exit(EXIT_FAILURE);
 	}
 	pop(head);
--- swap_callback.c
+++ swap_callback.c
@@ -8,7 +8,7 @@ void swap_callback(stack_t **head, unsigned int line_number)
 	if(*head == NULL || (*head)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		free_stack(head);
+		free_state(g_state);
 		exit(EXIT_FAILURE);
 	}
 	a = peek(head);
