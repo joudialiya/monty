f3ff9bb44edc8f747783839bf0f3fd21681049e4
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-20 07:44:22 -0700

    4. fixing error msg

--- pop_callback.c
+++ pop_callback.c
@@ -5,7 +5,7 @@ void pop_callback(stack_t **head, unsigned int line_number)
 {
 	if(*head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		free_state(g_state);
 		exit(EXIT_FAILURE);
 	}
