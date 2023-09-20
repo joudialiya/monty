60623eea86621fa34582b4bd25e022f56e10fd26
joudialiya[lagzal] <mlagzal.sayhi@gmail.com>
2023-09-20 10:04:18 -0700

    11. we nned to peek the number from the stack not the number from the head of the linked list

--- pchar_callback.c
+++ pchar_callback.c
@@ -3,17 +3,20 @@
 
 void pchar_callback(stack_t **head, unsigned int line_number)
 {
+	int n = 0;
+
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		free_state(g_state);
 		exit(EXIT_FAILURE);
 	}
-	if (((*head)->n < 0) || ((*head)->n > 127))
+	n = peek(head);
+	if ((n < 0) || (n > 127))
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		free_state(g_state);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n", (*head)->n);
+	printf("%c\n", n);
 }
